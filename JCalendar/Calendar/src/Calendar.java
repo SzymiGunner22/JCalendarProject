18e8e69962c10e50f1a90750f2a23d8ca0788f90
Szymon <s.rutkowski235@gmail.com>
2019-05-28 11:38:11 +0200

    Calendar

--- JCalendar/Calendar/src/Calendar.java
+++ JCalendar/Calendar/src/Calendar.java
@@ -1,4 +1,21 @@
-
+import java.util.*;
 public class Calendar {
+	 
+	ArrayList<Event> EventStorage =  new ArrayList();
+	
+void addEvent(Event e) {
+	EventStorage.add(e);
+	sortBystartTime();
+}
+void deleteEvent(Event e) {
+	EventStorage.remove(e);
+	sortBystartTime();
+}
+void sortBystartTime()
+{
+	Collections.sort(EventStorage);
+}
+
+
 
 }
